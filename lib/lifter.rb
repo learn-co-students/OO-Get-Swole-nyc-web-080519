3dced9ff81635c23602e06c09c1f32179e131455
DanBehrman <dan.behrman@wework.com>
2019-08-12 21:45:59 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,47 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def total_cost
+    memberships.map do |membership|
+      membership.cost
+    end.sum
+  end
+
+  def add_gym(membership_fee, gym)
+    Membership.new(membership_fee, self, gym)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.lifts
+    all.map do |lifter|
+      lifter.lift_total
+    end
+  end
+
+  def self.average_lift
+    lifts.sum / lifts.count
   end
 end
